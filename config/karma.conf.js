3d8062e6c6571743199cc1efd41c3489d31b59cf
timestep <jbosshellokitty@gmail.com>
2013-08-19 14:01:06 -0400

    fixed tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,25 +1,24 @@
-module.exports = function(config) {
-	config.set({
-		basePath = '../';
+module.exports = function (config) {
+  config.set({
+    basePath: '../',
 
-		files = [
-		  frameworks = ["jasmine"],
-			frameworks = ["jasmine"],
-		  'app/lib/angular/angular.js',
-		  'app/lib/angular/angular-*.js',
-		  'test/lib/angular/angular-mocks.js',
-		  'app/js/**/*.js',
-		  'test/unit/**/*.js'
-		];
+    files: [
+		    'app/lib/angular/angular.js',
+        'app/lib/angular/angular-*.js',
+        'test/lib/angular/angular-mocks.js',
+        'app/js/**/*.js',
+        'test/unit/**/*.js'
+    ],
 
-		autoWatch = true;
+    frameworks: ["jasmine"],
 
-		browsers = ['Chrome'];
+    autoWatch: true,
 
-		junitReporter = {
-		  outputFile: 'test_out/unit.xml',
-		  suite: 'unit'
-		};
+    browsers: ['chromium-browser'],
+
+    junitReporter: {
+        outputFile: 'test_out/unit.xml',
+        suite: 'unit'
+    }
   });
 };
-
