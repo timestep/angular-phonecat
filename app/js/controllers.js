c9ee9a571b683481c3381ea9e078b46177b69b14
timestep <jbosshellokitty@gmail.com>
2013-08-19 13:29:30 -0400

    added controller and test for controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* Controllers */
+
+function PhoneListCtrl($scope){
+	$scope.phones=[
+		{"name":"Nexus S","snippet":"Fast just got faster with Nexus S."},
+		{"name":"Motorola XOOM with Wi-Fi","snippet":"The Next, Next Generation Tablet."},
+		{"name":"Motorola XOOM", "snippet":"The Next Next Generation Tablet"}
+	];
+}
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,6 +2,12 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
-
+describe('PhoneCat controllers', function() {
+	describe('PhoneListCtrl', function(){
+		it('should create "phones" model with 3 phones', function(){
+			var scope = {},
+				ctrl = new PhoneListCtrl(scope)
+			expect(scope.phones.length).toBe(3);
+		});
+	});
 });
